e268eb6d535f718a649b08d3cafdaeefcf3c44fd
Rituraj <goswami.rituraj03@gmail.com>
2025-10-05 20:56:36 +0530

    Rename wsgi variable to 'application' for Gunicorn

--- chiclane_project/wsgi.py
+++ chiclane_project/wsgi.py
@@ -1,11 +1,7 @@
-# chiclane_project/wsgi.py
-
 import os
 from django.core.wsgi import get_wsgi_application
 
-# Set the settings module for the 'wsgi' application.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chiclane_project.settings')
 
-# This is the crucial line Vercel is looking for.
-# It gets the Django WSGI application and assigns it to the variable 'app'.
-app = get_wsgi_application()
+# This is the line to change
+application = get_wsgi_application()
