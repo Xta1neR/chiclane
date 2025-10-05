207ed52bcb7963d0ef1925c02d0256859615d621
Rituraj <goswami.rituraj03@gmail.com>
2025-10-05 20:37:29 +0530

    Add build script for Vercel

---
+++ build_files.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Exit on error
+set -o errexit
+
+# Install dependencies
+pip install -r requirements.txt
+
+# Collect static files
+python3.9 manage.py collectstatic --noinput
+
+# Apply database migrations
+python3.9 manage.py migrate
